74faf3f21a960998da8b106c0c6a2b0ef1c56ec9
Azeem Subhani <azeemforall4406148@gmail.com>
2021-09-07 21:10:16 +0500

    added redux to the frontend and allow user to sign in and data stored in mongodb in user model

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ const UserSchema = new schema(
   {
     userName: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, select: false },
+    password: { type: String, required: true },
     isBanned: { type: Boolean, default: false },
     role: { type: String, default: "user", enum: ["user", "root"] },
   },
